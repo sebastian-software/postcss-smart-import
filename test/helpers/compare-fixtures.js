4f53c8f4f9c164fa31721834a59250922a8b5072
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-14 20:19:12 +0200

    Some code formatting + disabled eslint checks for 'test' script

--- test/helpers/compare-fixtures.js
+++ test/helpers/compare-fixtures.js
@@ -8,21 +8,25 @@ function read(name) {
   return fs.readFileSync("fixtures/" + name + ".css", "utf8")
 }
 
-module.exports = function(t, name, opts, postcssOpts, warnings) {
+module.exports = function(t, name, opts, postcssOpts, warnings)
+{
   opts = assign({ path: "fixtures/imports" }, opts)
   return postcss(atImport(opts))
     .process(read(name), postcssOpts || {})
-    .then(function(result) {
+    .then(function(result)
+    {
       var actual = result.css
       var expected = read(name + ".expected")
 
       // handy thing: checkout actual in the *.actual.css file
       fs.writeFile("fixtures/" + name + ".actual.css", actual)
       t.is(actual, expected)
-      if (!warnings) {
+
+      if (!warnings)
         warnings = []
-      }
-      result.warnings().forEach(function(warning, index) {
+
+      result.warnings().forEach(function(warning, index)
+      {
         t.is(
           warning.text,
           warnings[index],
