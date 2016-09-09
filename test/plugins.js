4f4ccf72aa64fb1d4c02c7a3f190dd5d7c451a49
Sebastian Werner <s.werner@sebastian-software.de>
2016-09-09 10:13:13 +0200

    Sorting rules to work around some postcss quirks where it seems to randomly process walks in different order

--- test/plugins.js
+++ test/plugins.js
@@ -32,7 +32,7 @@ test("should apply plugins to root", (t) =>
   .then(() =>
   {
     t.deepEqual(atRules, [ "import" ])
-    t.deepEqual(rules, [ "bar", "foo" ])
+    t.deepEqual(rules.sort(), [ "bar", "foo" ].sort())
   })
 })
 
